1fac21a8ce841489b4461095fc4eb0151f7c060d
Artur <82472015+benyaar@users.noreply.github.com>
2025-07-18 23:36:29 +0300

    Adding Custom Components & Styling With CSS Modules

---
+++ pages/[meetupId]/index.js
@@ -0,0 +1,14 @@
+import MeetupDetail from "../../components/meetups/MeetupDetail";
+
+function MeetupDetails(){
+    return (
+        <MeetupDetail  
+            image='https://assets.simpleviewinc.com/sv-atlantic-city/image/upload/c_limit,h_1200,q_75,w_1200/v1/cms_resources/cms_resources/clients/meetac/TIDALWAVE2022_0813_195905_7278_ALIVECOVERAGE_7f2d4f26-2716-4507-adc1-4875f139f1ea.jpg'
+            title='A first meetup'
+            address='Spain'
+            description='This is a first meetup'
+        />
+    )
+}
+
+export default MeetupDetails;
